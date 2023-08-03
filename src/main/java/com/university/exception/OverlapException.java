1f34baa7730bd12a02699a134d38b9fd9b470155
lms8262 <zergling8262@gmail.com>
2023-08-03 13:21:03 +0900

    8.3 13:21

--- src/main/java/com/university/exception/OverlapException.java
+++ src/main/java/com/university/exception/OverlapException.java
@@ -2,7 +2,8 @@ package com.university.exception;
 
 public class OverlapException extends RuntimeException {
 	
-	// 수강신청시 중복되는 경우일때 발생(이미 신청한 강의, 같은 강의코드, 시간 겹침)
+	// 수강신청시 중복되는 경우(이미 신청한 강의, 같은 강의코드, 시간 겹침)
+	// 그 외 신규등록, 정보수정시 중복되는 경우
 	public OverlapException(String message) {
 		super(message);
 	}
